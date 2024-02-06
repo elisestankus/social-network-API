c894b33099c4a3a8b13b38d70ef30fce56528771
estan <elise.stankus@gmail.com>
2024-02-06 13:24:43 -0800

    added getThoughts and getSingleThought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,10 +2,29 @@ const { User, Thought } = require('../models');
 
 module.exports = {
     //get all thoughts
-
+    async getThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find();
+            res.json(thoughts);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
 
     //get a single thought by _id
-
+    async getSingleThought(req, res) {
+        try {
+            const thought = await Thought.findOne({_id: req.params.thoughtId});
+            if(!thought) {
+                return res.status(404).json({message: 'No thought found with this id!'});
+            }
+            res.json(thought);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
 
     //create a new thought
 
