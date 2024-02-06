c9a72611ae65418e3f347255a2bf17bdb2e96f55
estan <elise.stankus@gmail.com>
2024-02-06 12:46:15 -0800

    added createUser, updateUser, and deleteUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -30,13 +30,49 @@ module.exports = {
     },
 
     //create a new user
-
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err)
+        }
+    },
 
     //update a single user by _id
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                {_id: req.params.userId},
+                {$set: req.body},
+                {runValidators: true, new: true}
+            );
 
+            if(!user) {
+                return res.status(404).json({message: 'No user found with this id!'})
+            }
+
+            res.json(user);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err)
+        }
+    },
 
     //delete a single user by _id
-
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete({_id: req.params.userId})
+            if(!user) {
+                return res.status(404).json({message: 'No user found with this id!'})
+            }
+            res.json({message: 'User deleted'})
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
     
     //add a new friend to user's friend list
 
