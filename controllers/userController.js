f08dc5e7110e9e3b7a379bf8ad717f9424eddb5c
estan <elise.stankus@gmail.com>
2024-02-06 12:15:54 -0800

    outlined module exports

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,23 @@
 const { User, Thought } = require('../models');
+
+module.exports = {
+    //get all users
+
+
+    //get a single user by _id
+
+
+    //create a new user
+
+
+    //update a single user by _id
+
+
+    //delete a single user by _id
+
+    
+    //add a new friend to user's friend list
+
+
+    //delete a friend from a user's friend list
+}
