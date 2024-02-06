8339985935216edde785d32665148f6120082099
estan <elise.stankus@gmail.com>
2024-02-05 22:56:03 -0800

    added userSchema

--- models/User.js
+++ models/User.js
@@ -1,2 +1,39 @@
 const {Schema, model} = require('mongoose');
 
+//Schema for User model
+
+const userSchema = new Schema (
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email address']
+
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }   
+);
