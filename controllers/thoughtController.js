aec0c74c6e93826f9ec75b9d3a98318680e10555
estan <elise.stankus@gmail.com>
2024-02-06 13:16:06 -0800

    required models, outlined module exports

---
+++ controllers/thoughtController.js
@@ -0,0 +1,23 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    //get all thoughts
+
+
+    //get a single thought by _id
+
+
+    //create a new thought
+
+
+    //update a single thought by _id
+
+
+    //delete a single thought by _id
+
+
+    //add a new reaction to thought's reaction array
+
+
+    //delete a reaction from a thought's reaction array
+}
