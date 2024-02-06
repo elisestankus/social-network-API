caf4a90884563cb714861bdcf4e98f6b59de793b
estan <elise.stankus@gmail.com>
2024-02-05 23:24:02 -0800

    added thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,3 +1,30 @@
 const {Schema, model} = require('mongoose');
 const reactionSchema = require('./Reaction');
 
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // add getter to format
+        },
+        username: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+        reactions: [reactionSchema],
+    },
+    {
+            toJSON: {
+                virtuals: true,
+        }
+    },
+)
