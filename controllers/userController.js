4c5501d0fbd4b34ce53749e6c2c40627e92d42c8
estan <elise.stankus@gmail.com>
2024-02-06 12:28:51 -0800

    added getUsers and getSingleUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,10 +2,32 @@ const { User, Thought } = require('../models');
 
 module.exports = {
     //get all users
-
+    async getUsers(req, res) {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err)
+        }
+    },
 
     //get a single user by _id
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({_id: req.params.userId})
+            .populate('thoughts')
+            .populate('friends');
 
+            if(!user) {
+                return res.status(404).json({message: 'No user found with this id'})
+            }
+            res.json(user);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
 
     //create a new user
 
