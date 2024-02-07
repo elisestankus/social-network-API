05ec78dc6c106b8f33ef504f8baed6a4f42f37ec
estan <elise.stankus@gmail.com>
2024-02-06 19:58:00 -0800

    remove comment from createdAt field

--- models/Reaction.js
+++ models/Reaction.js
@@ -17,8 +17,7 @@ const reactionSchema = new Schema(
         },
         createdAt: {
             type: Date,
-            default: Date.now,
-            // add getter to format
+            default: Date.now
         },
     },
     {
