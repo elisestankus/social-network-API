e3a92f59b3dc6345324fee0d348e6a4a48ba7fd3
estan <elise.stankus@gmail.com>
2024-02-06 19:08:55 -0800

    required router and controllers

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
