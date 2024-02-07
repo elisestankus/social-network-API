2dda9bbf1d4050374978a53e76f58161b14bcb49
estan <elise.stankus@gmail.com>
2024-02-06 19:25:55 -0800

    added /thoughts routes and exported router

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -8,3 +8,21 @@ const {
     addReaction,
     deleteReaction
 } = require('../../controllers/thoughtController');
+
+// /api/thoughts
+
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
