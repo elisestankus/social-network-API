d3cbfde78309fb225cb5c71013149a980c5392d8
estan <elise.stankus@gmail.com>
2024-02-06 21:37:21 -0800

    created /users/userId/friends post route

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -17,8 +17,12 @@ router.route('/').get(getUsers).post(createUser);
 
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
+// /api/users/:userId/friends
+
+router.route('/:userId/friends').post(addFriend)
+
 // /api/users/:userId/friends/:friendId
 
-router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+router.route('/:userId/friends/:friendId').delete(deleteFriend);
 
 module.exports = router;
