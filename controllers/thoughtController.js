924a03a532718d6cc33135e3f3b0f0d81ec0abcc
estan <elise.stankus@gmail.com>
2024-02-06 17:45:34 -0800

    added createThought, updateThought, and deleteThought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -27,13 +27,65 @@ module.exports = {
     },
 
     //create a new thought
-
+    async createThought(req, res) {
+        try {
+            const thought = await Thought.create(req.body);
+            const user = await User.findOneAndUpdate(
+                {_id: req.body.userId},
+                {$push: {thoughts: thought._id}},
+                {new: true}
+            );
+            if(!user) {
+                return res.status(404).json({message: 'Thought created, but no user found with this id'})
+            }
+            res.json(thought);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
 
     //update a single thought by _id
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                {_id: req.params.thoughtId},
+                {$set: req.body},
+                {runValidators: true, new: true} 
+            )
 
+            if(!thought) {
+                return res.status(404).json({message: 'No thought found with this id!'})
+            }
+
+            res.json(thought)
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err)
+        }
+    },
 
     //delete a single thought by _id
-
+    async deleteThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndDelete({_id: req.params.thoughtId})
+            if(!thought) {
+                return res.status(404).json({message: 'No thought found with this id!'})
+            }
+            const user = await User.findOneAndUpdate(
+                {thoughts: req.params.thoughtId},
+                {$pull: {thoughts: req.params.thoughtId}},
+                {new: true}
+            );
+            if(!user) {
+                return res.status(404).json({message: 'Thought deleted but not removed from user thought array'})
+            }
+            res.json({message: 'Thought deleted'})
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    }
 
     //add a new reaction to thought's reaction array
 
