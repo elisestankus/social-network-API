0494033bf5b3cd89113532d3d8d8533d8a044295
estan <elise.stankus@gmail.com>
2024-02-06 18:11:07 -0800

    required router and controllers

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/userController');
+
