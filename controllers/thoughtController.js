f9b35b86aff26980810fe1f7d8e44d95064172d2
estan <elise.stankus@gmail.com>
2024-02-06 18:01:19 -0800

    added addReaction and deleteReaction

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -85,10 +85,40 @@ module.exports = {
             console.log(err);
             res.status(500).json(err);
         }
-    }
+    },
 
     //add a new reaction to thought's reaction array
-
+    async addReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                {_id: req.params.thoughtId},
+                {$addToSet: {reactions: req.body}},
+                {runValidators: true, new: true}
+            );
+            if(!thought) {
+                return res.status(404).json({message: 'No thought found with this id!'})
+            }
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
 
     //delete a reaction from a thought's reaction array
+    async deleteReaction(res, req) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                {_id: req.params.thoughtId},
+                {$pull: {reactions: {reactionId: req.params.reactionId}}},
+                {runValidators: true, new: true}
+            );
+            if (!thought) {
+                return res.status(404).json({message: 'No thought found with this id!'})
+            }
+            res.json(thought)
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    }
 }
